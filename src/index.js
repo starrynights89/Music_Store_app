42b9d39feab8200a6b479e7a5933a88b7f07564e
Alexander Hartson <alexander-hartson@live.com>
2019-09-09 00:17:22 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Card extends React.Component {
+  render() {
+    const img = "img/strings.png";
+    const imgalt = "string";
+    const desc = "A very authentic and beautiful instrument!!";
+    const price = 100;
+    const productName = "Strings";
+    return (
+      <div className="col-md-6 col-lg-4 d-flex align-items-stretch">
+        <div className="card mb-3">
+          <img className="card-img-top" src={img} alt={imgalt} />
+          <div className="card-body">
+            <h4 className="card-title">{productName}</h4>
+            Price: <strong>{price}</strong>
+            <p className="card-text">{desc}</p>
+            <a href="#" className="btn btn-primary">Buy</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Card/>,document.getElementById('root'));
