7d1a3750e08938bb09c797cbc9c9d7c28dab10e0
Alexander Hartson <alexander-hartson@live.com>
2019-09-10 12:41:41 -0500

    setting up basic react index page

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom';
 
 class Card extends React.Component {
   render() {
-    const img = "img/strings.png";
-    const imgalt = "string";
-    const desc = "A very authentic and beautiful instrument!!";
-    const price = 100;
-    const productName = "Strings";
     return (
       <div className="col-md-6 col-lg-4 d-flex align-items-stretch">
         <div className="card mb-3">
-          <img className="card-img-top" src={img} alt={imgalt} />
+          <img className="card-img-top" src={this.props.img} alt={this.props.imgalt} />
           <div className="card-body">
-            <h4 className="card-title">{productName}</h4>
-            Price: <strong>{price}</strong>
-            <p className="card-text">{desc}</p>
+            <h4 className="card-title">{this.props.productName}</h4>
+            Price: <strong>{this.props.price}</strong>
+            <p className="card-text">{this.props.desc}</p>
             <a href="#" className="btn btn-primary">Buy</a>
           </div>
         </div>
@@ -24,4 +19,42 @@ class Card extends React.Component {
   }
 }
 
-ReactDOM.render(<Card/>,document.getElementById('root'));
+class CardContainer extends React.Component {
+  constructor(props) {
+    // pass props to the parent component
+    super(props);
+    // initialize the state object for this component
+    this.state = {
+      cards: []
+    };
+  }
+
+  // Mount cards.json data file
+  componentDidMount() {
+    fetch('cards.json')
+    .then(res => res.json())
+    .then((result) => {
+      this.setState({
+        cards: result
+      });
+    });
+  }
+  render() {
+    const cards = this.state.cards;
+    let items = cards.map(
+      card => <Card key={card.id} {...card} />
+    )
+    return (
+      <div className='container pt-4'>
+        <h3 className='text-center text-primary'>Products</h3>
+        <div className="pt-4 row">
+          {items}
+        </div>
+      </div>
+    );
+  }
+} 
+
+ReactDOM.render(<CardContainer />,
+  document.getElementById('root')
+);
