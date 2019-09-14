98cf30a5443ba041bd7174ffc592f1ba70403111
Alexander Hartson <alexander-hartson@live.com>
2019-09-14 02:59:47 -0500

    setting up navigation classes

---
+++ src/Navigations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+export default class Navigation extends React.Component {
+  render() {
+    // The code to describe how our menu would look like
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
