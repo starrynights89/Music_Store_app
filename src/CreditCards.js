08697194f3f8cbb0513af4c74a4089a980498870
Alexander Hartson <alexander-hartson@live.com>
2019-09-15 22:01:34 -0500

    building credit card interface

--- src/CreditCards.js
+++ src/CreditCards.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { injectStripe, StripeProvider, Elements, CardElement } from 'react-stripe-elements';
 
-const INITIALSTATE = "INITIAL", SUCCESSSTATE = "COMPLETE", FAILEDSTATE = "FAILED";
+const INITIALSTATE = 'INITIAL', 
+  SUCCESSSTATE = 'COMPLETE', 
+  FAILEDSTATE = 'FAILED';
 
 // Child component
 class CreditCardForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      status: INITIALSTATE
+    };    
   }
   renderCreditCardInformation() {}
   renderSuccess() {}
   renderFailure() {}
+
+  render() {
+    let body = null;
+    switch (this.state.status) {
+    case SUCCESSSTATE:
+      body = this.renderSuccess();
+      break;
+    case FAILEDSTATE:
+      body = this.renderFailure();
+      break;
+    default:
+      body = this.renderCreditCardInformation();
+    }
+    
+    return (
+      <div>
+        {body}
+      </div>
+    );
+  }
 }
