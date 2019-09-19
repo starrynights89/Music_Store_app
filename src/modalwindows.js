bee8152487431766b131425398a81909a76d81e2
Alexander Hartson <alexander-hartson@live.com>
2019-09-19 10:52:00 -0500

    creating sign in form

--- src/modalwindows.js
+++ src/modalwindows.js
@@ -2,6 +2,67 @@ import React from 'react';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import CreditCardInformation from './CreditCards';
 
+class SignInForm extends React.Component {
+  constructor() {
+    super(props);
+    // this method will get called whenever a user input data into our form
+    this.handleChange = this.handleChange.bind(this);
+    // this method will get called whenever the HTML form gets submitted
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      errormessage = ''
+    };
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state));
+  }
+
+  render() {
+    // error message
+    let message = null;
+    // if the state contains an error message, show an error
+    if (this.state.errormessage.length !== 0) {
+      message = <h5 className="mb-4 text-danger">{this.state.errormessage}</h5>
+    }
+
+    return (
+      <div>
+        {message}
+        <form onSubmit={this.handleSubmit}>
+          <h5 className="mb-4">Basic Info</h5>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input name="email" type="email" className="form-control" id="email" onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="pass">Password:</label>
+            <input name="password" type="password" className="form-control" id="pass" onChange={this.handleChange} />
+          </div>
+          <div className="form-row text-center">
+            <div className="col-12 mt-2">
+              <button type="submit" className="btn btn-success btn-large">Sign In</button>
+            </div>
+            <div className="col-12 mt-2">
+              <button type="submit" className="btn btn-link text-info" 
+                onClick={() => this.props.handleNewUser()}> New User? Register</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
 export function BuyModalWindow(props) {
   return (
     <Modal id="buy" tabIndex="-1" role="dialog" isOpen={this.props.showModal} toggle={props.toggle}>
